0b9baa3f1adad23f491ab12d4edaca94c7154f3b
Olivier DALET <olivier.dalet@addupsolutions.com>
2022-03-18 10:55:00 +0100

    1st commit

---
+++ SonarIssue/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SonarIssue
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            var test = new TestClass();
+            test.DoSomething();
+        }
+    }
+
+    internal sealed class TestClass
+    {
+        public void DoSomething() => DoSomethingWithParameter(42);
+
+        private void DoSomethingWithParameter(int value = 0)
+        {
+            var positive = value >= 0 ? "positive" : "negative";
+            Console.WriteLine($"value is {positive}");
+        }
+    }
+}
